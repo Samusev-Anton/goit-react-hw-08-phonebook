3dd3ee8a24f827ef28ede94d090223e08a789444
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-01-11 08:11:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-07-phonebook">
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles} />
         <Provider store={store}>
